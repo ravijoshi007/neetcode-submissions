9335b4d6695ea7cb6f9d456af389bb4f8090e879
ravijoshi007 <joshi.ravi240@gmail.com>
2026-04-15 19:35:30 +0530

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.java
@@ -0,0 +1,29 @@
+class KthLargest {
+
+    private PriorityQueue<Integer> minHeap;
+    private int size;
+
+    public KthLargest(int k, int[] nums) {
+        size = k;
+        minHeap = new PriorityQueue<>();
+
+        for(int i = 0; i< nums.length; i++){
+            minHeap.offer(nums[i]);
+        }
+
+        while(minHeap.size() > k){
+            minHeap.poll();
+        }
+        
+    }
+    
+    public int add(int val) {
+
+        minHeap.offer(val);
+
+        if(minHeap.size() > size){
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
