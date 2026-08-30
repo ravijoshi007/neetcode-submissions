d48208b259d86db54595773e7e95d288deaecb04
ravijoshi007 <joshi.ravi240@gmail.com>
2026-08-30 21:52:11 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+
+        findSubsets(nums, 0, subset, result);
+        return result;
+        
+    }
+
+    private void findSubsets(int[] nums, int i, List<Integer>
+    subset, List<List<Integer>> result){
+
+        if( i >= nums.length){
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        findSubsets(nums, i+1, subset, result);
+        subset.remove(subset.size()-1);
+        findSubsets(nums, i+1, subset, result);
+
+    }
+}
